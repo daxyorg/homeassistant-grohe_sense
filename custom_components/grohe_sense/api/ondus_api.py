d5c600c8f72c55b0fe3bfdecc44eda6f1bb24c9f
Flo-Schilli <github@flo.schilli.dev>
2024-07-07 09:46:01 +0200

    Added documentation for api

--- custom_components/grohe_sense/api/ondus_api.py
+++ custom_components/grohe_sense/api/ondus_api.py
@@ -18,67 +18,164 @@ class OndusApi:
         self._session = session
 
     async def get_dashboard(self) -> Locations:
+        """
+        Get the dashboard information.
+        These dashboard information include most of the data which can also be queried by the appliance itself
+
+        :return: The locations information obtained from the dashboard.
+        :rtype: Locations
+        """
         url = f'{self.__api_url}/dashboard'
         data = await self._session.get(url)
         return Locations.from_dict(data)
 
     async def get_locations(self) -> List[Location]:
+        """
+        Get a list of locations.
+
+        :return: A list of Location objects.
+        :rtype: List[Location]
+        """
         url = f'{self.__api_url}/locations'
         data = await self._session.get(url)
-        locations = []
-        for location in data:
-            locations.append(Location.from_dict(location))
-        return locations
+        return [Location.from_dict(location) for location in data]
 
     async def get_rooms(self, location_id: string) -> List[Room]:
+        """
+        Get the rooms for a given location.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :return: A list of Room objects representing the rooms.
+        :rtype: List[Room]
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms'
         data = await self._session.get(url)
-        rooms = []
-        for room in data:
-            rooms.append(Room.from_dict(room))
-        return rooms
+        return [Room.from_dict(room) for room in data]
 
     async def get_appliances(self, location_id: string, room_id: string) -> List[Appliance]:
+        """
+        Get a list of appliances for a given location and room.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :return: A list of Appliance objects representing the appliances in the specified location and room.
+        :rtype: List[Appliance]
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances'
         data = await self._session.get(url)
-        appliances = []
-        for appliance in data:
-            appliances.append(Appliance.from_dict(appliance))
-        return appliances
+        return [Appliance.from_dict(appliance) for appliance in data]
 
     async def get_appliance_info(self, location_id: string, room_id: string, appliance_id: string) -> Appliance:
+        """
+        Get information about an appliance.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :return: The information of the appliance.
+        :rtype: Appliance
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}'
         data = await self._session.get(url)
-        return Appliance.from_json(data)
+        return Appliance.from_dict(data)
 
     async def get_appliance_details(self, location_id: string, room_id: string, appliance_id: string) -> Appliance:
+        """
+        Get details of an appliance by location, room, and appliance IDs.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :return: Appliance object containing the details.
+        :rtype: Appliance
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}/details'
         data = await self._session.get(url)
-        return Appliance.from_json(data)
+        return Appliance.from_dict(data)
 
     async def get_appliance_status(self, location_id: string, room_id: string, appliance_id: string) -> List[Status]:
+        """
+        Get the status of an appliance.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :return: A list of Status objects representing the current status of the appliance.
+        :rtype: List[Status]
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}/status'
         data = await self._session.get(url)
-        return [Status.from_json(state) for state in data]
+        return [Status.from_dict(state) for state in data]
 
     async def get_appliance_command(self, location_id: string, room_id: string, appliance_id: string) \
             -> ApplianceCommand:
+        """
+        Get possible commands for an appliance.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :return: The command for the specified appliance.
+        :rtype: ApplianceCommand
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}/command'
         data = await self._session.get(url)
-        return ApplianceCommand.from_json(data)
+        return ApplianceCommand.from_dict(data)
 
     async def get_appliance_notifications(self, location_id: string, room_id: string, appliance_id: string) \
             -> List[Notification]:
+        """
+        Returns the notifications associated with a specific appliance in a given room at a specific location.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :return: A list of Notification objects.
+        :rtype: List[Notification]
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}/notifications'
         data = await self._session.get(url)
-        notifications = []
-        for notification in data:
-            notifications.append(Notification.from_json(notification))
-        return notifications
+        return [Notification.from_dict(notification) for notification in data]
 
     async def get_appliance_data(self, location_id: string, room_id: string, appliance_id: string,
                                  from_date: Optional[datetime] = None, to_date: Optional[datetime] = None,
                                  group_by: Optional[OndusGroupByTypes] = None) -> MeasurementData:
+        """
+        Retrieves aggregated data for a specific appliance within a room.
+
+        :param location_id: ID of the location containing the appliance.
+        :type location_id: str
+        :param room_id: ID of the room containing the appliance.
+        :type room_id: str
+        :param appliance_id: ID of the appliance to get details for.
+        :type appliance_id: str
+        :param from_date: (optional) The starting date and time to retrieve data from. Defaults to None.
+        :type from_date: datetime
+        :param to_date: (optional) The ending date and time to retrieve data to. Defaults to None.
+        :type to_date: datetime
+        :param group_by: (optional) The time period for grouping the data. Defaults to None.
+        :type group_by: OndusGroupByTypes
+        :return: The aggregated measurement data for the specified appliance.
+        :rtype: MeasurementData
+        """
         url = f'{self.__api_url}/locations/{location_id}/rooms/{room_id}/appliances/{appliance_id}/data/aggregated'
         params = dict()
 
@@ -93,4 +190,4 @@ class OndusApi:
             url += '?' + urllib.parse.urlencode(params)
 
         data = await self._session.get(url)
-        return MeasurementData.from_json(data)
+        return MeasurementData.from_dict(data)
