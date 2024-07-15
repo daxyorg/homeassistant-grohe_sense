a0b67fd7e940ba1e0590e298402ab5cf87a2f380
Flo-Schilli <github@flo.schilli.dev>
2024-07-15 21:44:44 +0200

    Updated to use data coordinator per device

--- custom_components/grohe_sense/entities/grohe_sense_notifications.py
+++ custom_components/grohe_sense/entities/grohe_sense_notifications.py
@@ -1,24 +1,30 @@
 from datetime import timedelta
 from typing import List
 
+from homeassistant.core import callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import Throttle
 
 from custom_components.grohe_sense.api.ondus_api import OndusApi
 from custom_components.grohe_sense.dto.grohe_device import GroheDevice
 from custom_components.grohe_sense.dto.ondus_dtos import Notification
 from custom_components.grohe_sense.entities.configuration.grohe_entity_configuration import SensorTypes
+from custom_components.grohe_sense.entities.grohe_update_coordinator import GroheUpdateCoordinator
 
 NOTIFICATION_UPDATE_DELAY = timedelta(minutes=1)
 
 
-class GroheSenseNotificationEntity(Entity):
-    def __init__(self, domain: str, api: OndusApi, device: GroheDevice, sensor_type: SensorTypes):
-        self._api = api
+class GroheSenseNotificationEntity(CoordinatorEntity, Entity):
+    def __init__(self, domain: str, coordinator: GroheUpdateCoordinator, device: GroheDevice, sensor_type: SensorTypes):
+        super().__init__(coordinator)
+
+        self._coordinator = coordinator
         self._domain: str = domain
         self._device = device
         self._sensor_type = sensor_type
+        self._value: str | None = None
         self._notifications: List[Notification] = []
 
         self._attr_name = f'{self._device.name} {self._sensor_type.value}'
@@ -37,25 +43,10 @@ class GroheSenseNotificationEntity(Entity):
 
     @property
     def state(self):
-        def truncate_string(l, s):
-            if len(s) > l:
-                return s[:l - 4] + ' ...'
-            return s
+        return self._value
 
-        notifications = [notification for notification in self._notifications if notification.is_read is False]
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self._value = self._coordinator.data.notification
+        self.async_write_ha_state()
 
-        if len(notifications) > 0:
-            return f'{notifications[0].notification_text}'
-        else:
-            return 'No notifications'
-
-    @Throttle(NOTIFICATION_UPDATE_DELAY)
-    async def async_update(self):
-        # Reset notifications
-        self._notifications = []
-
-        self._notifications = await self._api.get_appliance_notifications(self._device.location_id,
-                                                                          self._device.room_id,
-                                                                          self._device.appliance_id)
-
-        self._notifications.sort(key=lambda n: n.timestamp, reverse=True)
--- custom_components/grohe_sense/sensor.py
+++ custom_components/grohe_sense/sensor.py
@@ -9,8 +9,8 @@ from .dto.grohe_device import GroheDevice
 from .entities.configuration.grohe_entity_configuration import GROHE_ENTITY_CONFIG, SensorTypes
 from .entities.grohe_sense import GroheSenseEntity
 from .entities.grohe_sense_guard import GroheSenseGuardWithdrawalsEntity
-from .entities.grohe_sense_guard_reader import GroheSenseGuardReader
 from .entities.grohe_sense_notifications import GroheSenseNotificationEntity
+from .entities.grohe_update_coordinator import GroheUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,32 +18,26 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     _LOGGER.debug(f'Adding sensor entities from config entry {entry}')
 
-    if DOMAIN not in hass.data or 'devices' not in hass.data[DOMAIN]:
-        _LOGGER.error(
-            "Did not find shared objects. You may need to update your configuration (this module should no longer be "
-            "configured under sensor).")
-        return
-
     ondus_api: OndusApi = hass.data[DOMAIN]['session']
 
     entities: List[GroheSenseNotificationEntity | GroheSenseEntity | GroheSenseGuardWithdrawalsEntity] = []
     devices: List[GroheDevice] = hass.data[DOMAIN]['devices']
 
     for device in devices:
-        reader = GroheSenseGuardReader(ondus_api, device)
-        await reader.async_update()
+        coordinator = GroheUpdateCoordinator(hass, device, ondus_api)
 
         if device.type in GROHE_ENTITY_CONFIG:
             for sensors in GROHE_ENTITY_CONFIG.get(device.type):
                 _LOGGER.debug(f'Attaching sensor {sensors} to device {device}')
                 if sensors == SensorTypes.WATER_CONSUMPTION:
-                    entities.append(GroheSenseGuardWithdrawalsEntity(DOMAIN, reader, device, sensors, 1))
-                    entities.append(GroheSenseGuardWithdrawalsEntity(DOMAIN, reader, device, sensors, 7))
+                    entities.append(GroheSenseGuardWithdrawalsEntity(DOMAIN, coordinator, device, sensors))
                 elif sensors == SensorTypes.NOTIFICATION:
-                    entities.append(GroheSenseNotificationEntity(DOMAIN, ondus_api, device, sensors))
+                    entities.append(GroheSenseNotificationEntity(DOMAIN, coordinator, device, sensors))
                 else:
-                    entities.append(GroheSenseEntity(DOMAIN, reader, device, sensors))
+                    entities.append(GroheSenseEntity(DOMAIN, coordinator, device, sensors))
         else:
             _LOGGER.warning('Unrecognized appliance %s, ignoring.', device)
+        await coordinator.async_request_refresh()
+
     if entities:
-        async_add_entities(entities)
+        async_add_entities(entities, update_before_add=True)
