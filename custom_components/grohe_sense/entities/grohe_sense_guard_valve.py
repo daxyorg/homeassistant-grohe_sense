82ae530dc77e57dfa702dad6a2917dd5aea88ca0
Flo-Schilli <github@flo.schilli.dev>
2024-09-04 20:10:12 +0200

    #12 Fixed an error on get_command if no response was provided

--- custom_components/grohe_sense/entities/grohe_sense_guard_valve.py
+++ custom_components/grohe_sense/entities/grohe_sense_guard_valve.py
@@ -54,7 +54,7 @@ class GroheSenseGuardValve(ValveEntity):
                                                                           self._device.room_id,
                                                                           self._device.appliance_id)
 
-        if command_response.command.valve_open is not None:
+        if command_response and command_response.command and command_response.command.valve_open:
             self._is_closed = not command_response.command.valve_open
         else:
             _LOGGER.error('Failed to parse out valve_open from commands response: %s', command_response)
